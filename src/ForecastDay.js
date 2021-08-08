6ee47fb74b94c017f4b69fb4c842424e244d14b3
gmgercke <gmgercke@zoho.com>
2021-08-08 02:12:31 +0200

    Update ForecastDay.js

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -12,13 +12,6 @@ export default function ForecastDay(props) {
     return `${minTemp}°`;
   }
 
-  function formatDay(timestamp) {
-    const date = new Date(timestamp * 1000);
-    const day = date.getDay();
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    return days[day];
-  }
-
   return (
     <div className="ForecastDay">
       <div className="forecast-day">{props.data.dt}</div>
